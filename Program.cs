b835fab45556526169410ceb385499d0c8279332
BilalyC <bilaly.camara@outlook.com>
2018-12-19 13:02:58 +0100

    Exercice 2 Complet

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exo2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Veillez taper votre âge :");
+            int age = int.Parse(Console.ReadLine());
+
+            if (age >= 18)
+                Console.WriteLine("Vous avez " + age + " ans, vous êtes donc majeur.");
+            else
+                Console.WriteLine("Vous avez " + age + " ans, vous êtes donc mineur.");
+        }
+    }
+}
